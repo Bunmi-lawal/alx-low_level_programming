e233aab6dd37f1d4bbca57ed556eb0661196650b
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-11 10:10:09 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints sizes of characters
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n")\n",sizeof (long int));
+	printf("size of a long long int: %zu byte(s)\n",sizeof(long long int));nt));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
