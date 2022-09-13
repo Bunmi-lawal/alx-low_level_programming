fc1bebc7d263331d125e2a972c9ab42aaa1e67fb
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-12 17:16:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  *  main - Entry point
  *  description: prints Programming is like building a multilingual puzzle
- *  Return:0
+ *  Return: (0)
  */
-
 int main(void)
-
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
-
+	puts("\nProgramming is like building a multilingual puzzle");
+	return (0);
 }
