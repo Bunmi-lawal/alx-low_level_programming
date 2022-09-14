1c250c4714f6cf06a2221849ff1a395888009407
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-14 16:55:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,23 @@
 /**
  * print_sign - function to print the sign of a number
  * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 for greater than zero, 0 for equal to 0, -1 for less than zero
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+_putchar(43);
 return (1);
 }
-else if (n == 0)
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else if (n = 0)
 {
 _putchar(48);
 return (0);
-}else if (n < 0)
-{
-_putchar('-');
-return (-1);
 }
 }
