6e112dc41b78cc5c52e39f6420e9f3b0f0097d1a
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-14 17:03:14 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * print_sign - function to print the sign of a number
  * @n: the int to check
@@ -16,7 +16,7 @@ else if (n < 0)
 _putchar(45);
 return (-1);
 }
-else if (n = 0)
+else
 {
 _putchar(48);
 return (0);
