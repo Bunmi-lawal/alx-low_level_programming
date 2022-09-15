6af00716fc37f56dfb08fa2667743528d8524089
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-14 17:36:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ * @b: number targeted
+ * Return: returns 0
+ */
+int _abs(int b)
+{
+if (b > 0)
+return (b);
+else if (b < 0)
+return (-b);
+else
+return (0);
+}
