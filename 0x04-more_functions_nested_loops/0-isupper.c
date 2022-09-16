354c3e451fdaac0efdf82e0fad864c1cce95364c
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-15 20:44:00 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check if character is upper or lowercase
+ * @c: character to be tested
+ * Return: 1 or 0 if otherwise
+ */
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+{
+return (1);
+return (0);
+}
