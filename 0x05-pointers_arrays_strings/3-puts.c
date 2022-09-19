18f0a80f53425b9cf127cc31a760e08ecc2b885d
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-19 15:55:39 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
 * _puts - function that pring a string
-* str : string to be printed
+* *str : string to be printed
 * return: a void answer
 */
 void _puts(char *str)
 {
-for (;*str != '\0'; str++)
+for (; *str != '\0'; str++)
 {
 putchar(*str);
 }
