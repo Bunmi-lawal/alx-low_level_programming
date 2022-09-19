5b0846592a2aa1dc10683da3f58ad0dfa6472aa1
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-19 16:17:26 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/** 
+*print_rev - function that prints a string in reverse
+*@s : string to be printed
+*return : void
+*/
+void print_rev(char *s)
+{
+b = 0
+while(s[b] != '\0')
+{
+b++;
+}
+for (b -= 1; b >= 0; b--)
+{
+_putchar(s[b]);
+}
+{
+_putchar('\n');
+}
+}
