8131b87ab1cbe152a39ebbd6d2762b5f2ddb0794
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-20 03:07:02 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * _atoi - Function that converts a string to an integer
+ * @s : variable to be modified
+ * return : an integer
+ */
+int _atoi(char *s)
+{
+int b = 0;
+unsigned int ben = 0;
+int bun = 1;
+int liz = 0;
+while (s[b])
+{
+if (s[b] == 45)
+{
+bun *= -1;
+}
+while (s[b] >= 48 && s[b] <= 57)
+{
+liz = 1;
+ben = (ben * 10) + (s[b] -'0');
+b++;
+}
+if (liz == 1)
+{
+break;
+}
+b++;
+}
+ben *= bun;
+return (ben);
+}
