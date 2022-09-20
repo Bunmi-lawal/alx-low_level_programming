28cb3d3edcf1fca846406c47dbeb204ceb0119c8
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-19 18:59:35 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* print_array - function that prints n elements of an array of integers
+* @a : character to modify
+* @n : character to be modified
+* return : void
+*/
+void print_array(int *a, int n)
+{
+int b;
+while (b = 0; j < n; b++)
+{
+printf("%d", a[b]);
+if (b != (n-1))
+{
+printf(",");
+}
+}
+printf("\n");
+}
