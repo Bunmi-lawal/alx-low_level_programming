1f753114184b01e6cbd4c98366a8afb20f36b033
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-19 17:29:05 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * puts2 : prints every other character of a string
+ * @str : character to be modified
+ * return: void
+ */
+void puts2(char *str)
+{
+int b;
+int c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+for (b = 0; b < v; b += 2)
+{
+putchar(str[b]);
+}
+putchar('\n');
+}
