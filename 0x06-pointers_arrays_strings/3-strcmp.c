6c7e5e4131c03ca387345d8cc6d3e2d1cd205754
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-21 15:19:37 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int bun = 0, i= 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+int bun, i;
+bun = 0;
+while (s1[bun] == s2[bun] && s2[bun] != '\0')
 {
-if (s1[i] != s2[i])
-{
-bun = 1;
-break;
+bun++;
 }
-i++;
-}
-if (bun == 0)
-return 0;
-else 
-return 1;
+i = s1[bun] - s2[bun]; 
+return (i);
 }
