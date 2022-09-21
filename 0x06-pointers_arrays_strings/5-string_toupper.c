815b3cb74301376046d209ae16c6edc841b40759
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-21 15:37:58 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * string_toupper - function to change lowercase of a string to uppercase
+ * @b: point to sring
+ * return : string to uppercase
+ */
+char *string_toupper(char *b)
+{
+int m = 0;
+while (b[m])
+{
+if (b[m] >= 97 && b[m] <= 122)
+{
+b[m] -= 32;
+}
+m++;
+}
+return (b);
+}
