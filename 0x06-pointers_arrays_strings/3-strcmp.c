7331aa7efbc4daf26fa0c01926fa891c4371d876
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-21 14:35:35 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - function that compares two string
+ * @s1 - first string
+ * @s2 - second string
+ * return - a string
+ */
+int _strcmp(char *s1, char *s2)
+{
+int bun = 0, i= 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+bun = 1;
+break;
+}
+i++;
+}
+if (bun == 0)
+return 0;
+else 
+return 1;
+}
