9ff2ce1aeeb046071763740cec8cf5bf24dd6ca3
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-23 17:41:09 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,9 +14,10 @@ p = &n;
  * You are not allowed to use the variable a in your new line of code
  * You are not allowed to modify the variable p
  * You can only write one statement
- * You are not allowed to use ,
+ * Your code should be written at line 19, before the ;
  */
-*(p + 5) = 98;
- printf("a[2] = %d\n", a[2]);
- return (0);
+*(p +5) = 98;
+/* this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
