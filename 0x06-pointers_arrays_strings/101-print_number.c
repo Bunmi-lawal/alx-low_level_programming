76170d625a94f5fa4baa084b3ca635a4218d511e
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-23 20:15:35 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,14 @@
  */
 void print_number(int n)
 {
-unsigned int num;
-num = n;
 if (n < 0)
 {
 putchar('_');
-num = -n;
+n = -n;
 }
-if ((num / 10) > 0)
+if (n / 10)
 {
-print_number(num / 10);
+print_number(n / 10);
 }
-putchar((num % 10) + '0');
+putchar(num % 10 + '0');
 }
