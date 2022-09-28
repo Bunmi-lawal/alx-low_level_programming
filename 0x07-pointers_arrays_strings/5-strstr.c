2660379290455dd444736ec72449f64e54474702
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-27 21:05:48 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,18 +7,19 @@
  */
 char *_strstr(char *haystack, char *needle); 
 {
-char *str1, *str2;
+char *str0, *str2;
 while (*haystack != '\0')
 {
-str1 = haystack;
+str0 = haystack;
 str2 = needle; 
 while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 {
-haystack++; str2++; 
+haystack++;
+str2++; 
 }
 if (*str2 == '\0')
-return (str1); 
-haystack = str1 + 1;
+return (str0); 
+haystack = str0 + 1;
 }
 return (0);
 }
