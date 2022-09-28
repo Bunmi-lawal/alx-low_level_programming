88b97ff870cae1ded6857ae0e34862b0ab34c4ff
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-09-27 21:22:05 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** 
+ * print_chessboard - function that prints the chessboard
+ * @a: pointer
+ * Return: void 
+ */ 
+void print_chessboard(char (*a)[8]) 
+{
+int b, c;
+b = 0;
+while (i < 8) 
+{
+c = 0;
+while (c < 8)
+{
+_putchar (a[b][c]);
+c++;
+}
+_putchar ('\n');
+b++;
+} 
+}
