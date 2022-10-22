9b281b0ba5ce3dd0d086e874158012fc072791ee
Bunmi-lawal <Elawalbunmi@gmail.com>
2022-10-21 22:16:21 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,12 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * list_len - function that returns count of elements
  * @h: pointer to the node
  * Return: @count the existing number of nodes
  */
+int main()
+{
 size_t list_len(const list_t *h)
 {
 int count = 0;
@@ -15,4 +18,5 @@ count++;
 current = current->next;
 }
 return (count);
+}
 }
