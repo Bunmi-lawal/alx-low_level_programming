a292257ca6267ed89f52c7f3689ba966c0d79b56
Bunmi lawal <elawalbunmi@gmail.com>
2023-07-09 14:07:15 -0700

    create main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include "main.h"
-#include <stdio.h>
-
-int main(void){
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -13,10 +9,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int _putchar(char c);
-}
+
 #endif /* MAIN_H_ */
-
-
-
-
