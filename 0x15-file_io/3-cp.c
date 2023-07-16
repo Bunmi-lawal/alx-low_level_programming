6d37b8225ec1ec4b638b4ee70b8cf22d9b9e2773
Bunmi lawal <elawalbunmi@gmail.com>
2023-07-15 19:28:33 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
 #define BUF_SIZE 1024
-
 /**
  * main - Entry point
  * @argc: Number of arguments
@@ -16,12 +14,13 @@ int main(int argc, char **argv)
 {
 int fd_from, fd_to, read_bytes, write_bytes;
 char buffer[BUF_SIZE];
+mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 if (fd_to == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 while ((read_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
