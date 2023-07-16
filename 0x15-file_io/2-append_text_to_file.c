e8b55a780c61f7526c8fdfc533a87e6c6acdc086
Bunmi lawal <elawalbunmi@gmail.com>
2023-07-15 17:15:48 -0700

    update code for 2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,32 +4,28 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with the given name and writes the text content
- * @filename: Name of the file to create
- * @text_content: NULL-terminated string to write to the file
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: Name of the file 
+ * @text_content: NULL-terminated string to add at the end of file
  * Return: 1 on success, -1 on failure
  *
- * Description: The function creates a file with the specified filename.If the
- * file already exists,its content will b truncated.The text_content is written
- * to the file.If text_content is NULL, an empty file is created.
- * The file will have permissions rw-------.
+ * Description: The function append text to the end of a file.If 
+ * text_content is NULL , nothing will be added
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-int file_descriptor;
-ssize_t bytes_written;
-size_t text_length = 0;
+int file_descriptor, bytes_written, length = 0;
 if (filename == NULL)
 return (-1);
-file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+file_descriptor = open(filename, O_WRONLY | O_APPEND);
 if (file_descriptor == -1)
 return (-1);
 if (text_content != NULL)
 {
-while (text_content[text_length] != '\0')
-text_length++;
-bytes_written = write(file_descriptor, text_content, text_length);
-if ((size_t) bytes_written != text_length)
+while (text_content[length])
+length++;
+bytes_written = write(file_descriptor, text_content, length);
+if (bytes_written != length)
 {
 close(file_descriptor);
 return (-1);
